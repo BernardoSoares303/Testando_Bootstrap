92305a77e8f3f4467b021821ac81349d487a051a
BernardoSoares303 <bbnado34@gmail.com>
2026-04-16 19:30:42 -0300

    Botoes Laterais Com Utilidade

---
+++ Assets/scripts.js
@@ -0,0 +1,16 @@
+function Compra(){
+    alert("Compra Registrada!");
+}
+
+
+function irYoutube() {
+    window.location.href = "https://www.youtube.com/";
+}
+
+function irOutlook(){
+    window.location.href = "https://outlook.office.com/"
+}
+
+function irSecundaria(){
+    window.location.href = "Pagina2.html"
+}
--- scripts.js
+++
@@ -1,3 +0,0 @@
-function Compra(){
-    alert("Compra Registrada!");
-}
